8985cb48cec782d5655ccc712764aed246879ff1
Peter Savichev (proton) <psavichev@gmail.com>
2020-12-01 21:28:06 +0100

    day 1 part 1

---
+++ 2020/1.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+require 'open-uri'
+
+numbers = open('1-inputs.txt').read.split.map(&:to_i)
+
+numbers.each do |x|
+numbers.each do |y|
+  next if x == y
+  puts x * y if x + y == 2020
+end
+end
