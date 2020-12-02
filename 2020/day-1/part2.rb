f91ad10bf0802657831bf801ff06c34a93b3ed57
Peter Savichev (proton) <psavichev@gmail.com>
2020-12-02 21:44:53 +0100

    split day 1 parts to different files

--- 2020/day-1/part2.rb
+++ 2020/day-1/part2.rb
@@ -5,16 +5,9 @@ numbers = open('input.txt').read.split.map(&:to_i)
 
 numbers.each do |x|
 numbers.each do |y|
-  next if x == y
-  puts "PART 1: #{x * y}" if x + y == 2020
-end
-end
-
-numbers.each do |x|
-numbers.each do |y|
 numbers.each do |z|
   next if x == y || y == z || z == x
-  puts "PART 2: #{x * y * z}" if x + y + z == 2020
+  puts x * y * z if x + y + z == 2020
 end
 end
 end
