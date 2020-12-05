3129f721eac731ed0d1ea76a6956b8f6a2c6e9bb
Peter Savichev (proton) <psavichev@gmail.com>
2020-12-06 00:45:37 +0100

    code cleaning

--- 2020/day-5/part1.scala
+++ 2020/day-5/part1.scala
@@ -3,35 +3,27 @@ import scala.io.Source
 
 object Part1 extends App {
   val filepath = "input.txt"
-  // val lines = Source.fromFile(filepath).getLines()
-  // for (line <- ) {
-  //   println(line + " " + Array(seatId(line)).mkString("-"))
-  // }
-  val maxSeat = Source.fromFile(filepath).getLines().map(seatId(_)).reduceLeft(_ max _)
+  val maxSeat = Source
+                  .fromFile(filepath)
+                  .getLines()
+                  .map(seatId(_))
+                  .reduceLeft(_ max _)
+
   println(maxSeat)
 
   def seatId(line: String): Int = {
-    var minRow = 0
+    var row = 0
     var rowCnt = 128
-    var minCol = 0
+    var col = 0
     var colCnt = 8
     for (char <- line) {
-      if (char == 'F') {
-        rowCnt /= 2
-      } else if (char == 'B') {
-        rowCnt /= 2
-        minRow += rowCnt
-      } else if (char == 'L') {
-        colCnt /= 2
-      } else if (char == 'R') {
-        colCnt /= 2
-        minCol += colCnt
+      char match {
+        case 'F' => rowCnt /= 2
+        case 'B' => rowCnt /= 2; row += rowCnt
+        case 'L' => colCnt /= 2
+        case 'R' => colCnt /= 2; col += colCnt
       }
     }
-    return minRow * 8 + minCol
+    return row * 8 + col
   }
 }
-
-// BFFFBBFRRR: row 70, column 7, seat ID 567.
-// FFFBBBFRRR: row 14, column 7, seat ID 119.
-// BBFFBBFRLL: row 102, column 4, seat ID 820
